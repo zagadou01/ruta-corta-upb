0d4f7a7d998c6f61bf4dccb7adac939801665528
zagadou01 <ju_na2705@outlook.com>
2025-04-28 02:31:54 -0500

    Project updates

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,12 @@
-module rutacortaupb.rutacortaupb {
+module core {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
 
-
-    opens view to javafx.fxml;
     exports view;
     exports controller;
+    exports model;
+
+    opens view to javafx.fxml;
     opens controller to javafx.fxml;
 }
