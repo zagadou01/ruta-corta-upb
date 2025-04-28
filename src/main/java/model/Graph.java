14feac96ab699a0708948da8c9839911a06029dd
zagadou01 <ju_na2705@outlook.com>
2025-04-27 19:49:41 -0500

    Add Dijkstra's shortest path algorithm

--- src/main/java/model/Graph.java
+++ src/main/java/model/Graph.java
@@ -67,4 +67,80 @@ public class Graph {
     public LinkedList<Building> getBuildings() {
         return buildings;
     }
+
+    public String[] shortestPath(String initialBuilding, String finalBuilding /*add boolean stairs*/) {
+        int initialIndex = buildings.getIndex(initialBuilding);
+        int finalIndex = buildings.getIndex(finalBuilding);
+
+        int[] distances = new int[buildings.getSize()];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+        distances[initialIndex] = 0;
+
+        boolean[] visitedNodes = new boolean[buildings.getSize()];
+        Arrays.fill(visitedNodes, false);
+
+        int currentBuilding = initialIndex;
+
+        while (!visitedNodes[currentBuilding]) {
+            visitedNodes[currentBuilding] = true;
+
+            for (int i = 0; i < routes[currentBuilding].length; i++) {
+                Route auxiliaryRoutes = routes[currentBuilding][i];
+                if (auxiliaryRoutes != null) {
+                    int distance = auxiliaryRoutes.getDistance() + distances[currentBuilding];
+                    if (distance < distances[i]) {
+                        distances[i] = distance;
+                    }
+                }
+            }
+
+            int auxiliaryCurrentDistance = Integer.MAX_VALUE;
+            for (int i = 0; i < distances.length; i++) {
+                if (!visitedNodes[i] && distances[i] != Integer.MAX_VALUE) {
+                    if (distances[i] < auxiliaryCurrentDistance) {
+                        auxiliaryCurrentDistance = distances[i];
+                        currentBuilding = i;
+                    }
+                }
+            }
+        }
+
+        int pathSize = 1;
+        String[] path = new String[pathSize];
+        path[0] = buildings.getName(finalIndex);
+
+        int currentDistance = distances[finalIndex];
+        currentBuilding = finalIndex;
+        while (currentDistance != 0) {
+            for (int i = 0; i < routes[currentBuilding].length; i++) {
+                Route auxiliaryRoutes = routes[currentBuilding][i];
+                if (auxiliaryRoutes != null) {
+                    int distance = currentDistance - auxiliaryRoutes.getDistance();
+                    if (distances[i] == distance) {
+                        currentDistance -= auxiliaryRoutes.getDistance();
+                        currentBuilding = i;
+                        path = Arrays.copyOf(path, pathSize + 1);
+                        path[pathSize] = buildings.getName(currentBuilding);
+                        pathSize++;
+                        break;
+                    }
+                }
+            }
+        }
+        path = reverse(path);
+
+        return path;
+    }
+
+    private String[] reverse(String[] array) {
+        String[] auxiliary = new String[array.length];
+        int j = 0;
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            auxiliary[j] = array[i];
+            j++;
+        }
+
+        return auxiliary;
+    }
 }
--- src/main/java/model/TerminalMain.java
+++ src/main/java/model/TerminalMain.java
@@ -3,32 +3,56 @@ package model;
 // TODO Main temporal para probar algoritmos y grafos por consola, se eliminará al final
 public class TerminalMain {
     public static void main(String[] args) {
+        /*
         Building a = new Building("A");
         Place cafeteria = new Place("Cafeteria");
         a.addPlace(cafeteria);
         System.out.println(a.getPlaces().toString());
+        */
 
-        /*
         Graph graph = new Graph();
 
         graph.addBuilding(new Building("a"));
         graph.addBuilding(new Building("b"));
         graph.addBuilding(new Building("c"));
         graph.addBuilding(new Building("d"));
+        graph.addBuilding(new Building("e"));
+        graph.addBuilding(new Building("f"));
+        graph.addBuilding(new Building("g"));
 
-        graph.addRoute("a", "b", 5, true);
-        graph.addRoute("b", "a", 5, true);
+        graph.addRoute("a", "b", 2, true);
+        graph.addRoute("a", "c", 6, true);
 
-        graph.addRoute("b", "d", 4, true);
-        graph.addRoute("d", "b", 4, true);
+        graph.addRoute("b", "a", 2, true);
+        graph.addRoute("b", "d", 5, true);
 
-        graph.addRoute("c", "d", 3, true);
-        graph.addRoute("d", "c", 3, true);
+        graph.addRoute("c", "a", 6, true);
+        graph.addRoute("c", "d", 8, true);
 
-        graph.addRoute("a", "c", 2, true);
-        graph.addRoute("c", "a", 2, true);
+        graph.addRoute("d", "b", 5, true);
+        graph.addRoute("d", "c", 8, true);
+        graph.addRoute("d", "e", 10, true);
+        graph.addRoute("d", "f", 15, true);
+
+        graph.addRoute("e", "d", 10, true);
+        graph.addRoute("e", "f", 6, true);
+        graph.addRoute("e", "g", 2, true);
+
+        graph.addRoute("f", "d", 15, true);
+        graph.addRoute("f", "e", 6, true);
+        graph.addRoute("f", "g", 6, true);
+
+        graph.addRoute("g", "e", 2, true);
+        graph.addRoute("g", "f", 6, true);
 
         graph.print();
-        */
+
+        String[] path = graph.shortestPath("a", "g");
+
+        System.out.print("Start -> ");
+        for (String s : path) {
+            System.out.print(s + " -> ");
+        }
+        System.out.print("End");
     }
 }
