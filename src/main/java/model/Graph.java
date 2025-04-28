8a0b86ff7ff28cd20f73ed05cdde1119be9efdf4
zagadou01 <ju_na2705@outlook.com>
2025-04-28 00:36:06 -0500

    Add removeBuilding()

--- src/main/java/model/Graph.java
+++ src/main/java/model/Graph.java
@@ -39,6 +39,62 @@ public class Graph {
         routes = auxiliar;
     }
 
+    public void removeBuilding(String building) {
+        // Eliminar rutas con el edificio
+        int buildingIndex = buildings.getIndex(building);
+        for (int i = 0; i < routes.length; i++) {
+            if (routes[buildingIndex][i] != null) {
+                String routeBuildingName = buildings.getName(i);
+                removeRoute(building, routeBuildingName);
+            }
+        }
+
+        // Eliminar edificio de la lista de edificios y reducir el tamaño
+        buildings.remove(buildingIndex);
+
+        // Reducir el tamaño de rutas y rellenar el espacio de la ruta eliminada
+        int size = buildings.getSize();
+        Route[][] auxiliaryRoutesMatrix = new Route[size][size];
+        for (int i = 0; i < buildingIndex; i++) {
+            Route[] array1 = Arrays.copyOfRange(routes[i], 0, buildingIndex);
+            Route[] array2 = Arrays.copyOfRange(routes[i], buildingIndex + 1, size + 1);
+
+            auxiliaryRoutesMatrix[i] = concatenateArrays(array1, array2);
+        }
+        for (int i = buildingIndex; i < size; i++) {
+            Route[] array1 = Arrays.copyOfRange(routes[i + 1], 0, buildingIndex);
+            Route[] array2 = Arrays.copyOfRange(routes[i + 1], buildingIndex + 1, size + 1);
+
+            auxiliaryRoutesMatrix[i] = concatenateArrays(array1, array2);
+        }
+        routes = auxiliaryRoutesMatrix;
+
+        // Actualizar el indice de los edificios luego del eliminado para que sigan en orden
+        int auxiliaryBuildingIndex = buildingIndex + 1;
+        Building auxiliaryBuilding = buildings.getNode(auxiliaryBuildingIndex);
+        while (auxiliaryBuilding != null) {
+            auxiliaryBuilding.setIndex(auxiliaryBuilding.getIndex() - 1);
+            auxiliaryBuildingIndex += 1;
+            auxiliaryBuilding = buildings.getNode(auxiliaryBuildingIndex);
+        }
+    }
+
+    // Concatena los dos arrays en los que se separan las rutas al eliminar una ruta
+    private Route[] concatenateArrays(Route[] array1, Route[] array2) {
+        int size = array1.length + array2.length;
+        Route[] concatenatedArray = new Route[size];
+
+        for (int i = 0; i < array1.length; i++) {
+            concatenatedArray[i] = array1[i];
+        }
+
+        for (int i = 0, j = array1.length; i < array2.length; i++, j++) {
+            concatenatedArray[j] = array2[i];
+        }
+
+        return concatenatedArray;
+    }
+
     public void addRoute(String initialBuilding, String finalBuilding, int distance, boolean stairs) {
         int initialIndex = buildings.getIndex(initialBuilding);
         int finalIndex = buildings.getIndex(finalBuilding);
@@ -105,7 +161,7 @@ public class Graph {
         while (!visitedNodes[currentBuilding]) {
             visitedNodes[currentBuilding] = true;
 
-            for (int i = 0; i < routes[currentBuilding].length; i++) {
+            for (int i = 0; i < routes.length; i++) {
                 Route auxiliaryRoutes = routes[currentBuilding][i];
                 if (auxiliaryRoutes != null) {
                     int distance = auxiliaryRoutes.getDistance() + distances[currentBuilding];
@@ -133,7 +189,7 @@ public class Graph {
         int currentDistance = distances[finalIndex];
         currentBuilding = finalIndex;
         while (currentDistance != 0) {
-            for (int i = 0; i < routes[currentBuilding].length; i++) {
+            for (int i = 0; i < routes.length; i++) {
                 Route auxiliaryRoutes = routes[currentBuilding][i];
                 if (auxiliaryRoutes != null) {
                     int distance = currentDistance - auxiliaryRoutes.getDistance();
