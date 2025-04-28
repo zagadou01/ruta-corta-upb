e3da2816ba1cced42c4076d9d8702a51cf47e4d3
zagadou01 <ju_na2705@outlook.com>
2025-04-28 02:30:36 -0500

    Testing FileController methods

--- src/main/java/model/TerminalMain.java
+++ src/main/java/model/TerminalMain.java
@@ -1,24 +1,29 @@
 package model;
 
+import controller.FileController;
+
 // TODO Main temporal para probar algoritmos y grafos por consola, se eliminará al final
 public class TerminalMain {
     public static void main(String[] args) {
-        /*
-        Building a = new Building("A");
-        Place cafeteria = new Place("Cafeteria");
-        a.addPlace(cafeteria);
-        System.out.println(a.getPlaces().toString());
-        */
+        Building a = new Building("0");
+        Place restaurant = new Place("Restaurant");
+        Place library = new Place("Library");
+        a.addPlace(restaurant);
+        a.addPlace(library);
 
         Graph graph = new Graph();
 
-        graph.addBuilding(new Building("0"));
+        /*
+        graph.addBuilding(a);
         graph.addBuilding(new Building("1"));
         graph.addBuilding(new Building("2"));
         graph.addBuilding(new Building("3"));
         graph.addBuilding(new Building("4"));
         graph.addBuilding(new Building("5"));
         graph.addBuilding(new Building("6"));
+        */
+
+        graph.addBuildings(FileController.readBuildings());
 
         graph.addRoute("0", "1", 2, true);
         graph.addRoute("0", "2", 6, true);
@@ -40,7 +45,7 @@ public class TerminalMain {
 
         graph.print();
 
-        System.out.println(graph.getBuildings().toString());
+        FileController.writeBuildings(graph.getBuildings());
     }
 
     private static void printPath(String[] path) {
