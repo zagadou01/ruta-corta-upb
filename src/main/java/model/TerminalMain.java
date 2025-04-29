ae1fb50eeb58bb5cf6d4ec63f7080a952b0970fc
zagadou01 <ju_na2705@outlook.com>
2025-04-29 08:22:49 -0500

    Test shortest path algorithm with and without stairs

--- src/main/java/model/TerminalMain.java
+++ src/main/java/model/TerminalMain.java
@@ -26,26 +26,27 @@ public class TerminalMain {
         graph.addBuildings(FileController.readBuildings());
 
         graph.addRoute("0", "1", 2, true);
-        graph.addRoute("0", "2", 6, true);
+        graph.addRoute("0", "2", 6, false);
         graph.addRoute("1", "3", 5, true);
-        graph.addRoute("2", "3", 8, true);
+        graph.addRoute("2", "3", 8, false);
         graph.addRoute("3", "4", 9/*10*/, true);
-        graph.addRoute("3", "5", 9/*15*/, true);
-        graph.addRoute("4", "5", 6, true);
-        graph.addRoute("4", "6", 2, true);
+        graph.addRoute("3", "5", 9/*15*/, false);
+        graph.addRoute("4", "5", 6, false);
+        graph.addRoute("4", "6", 2, false);
         graph.addRoute("5", "6", 6, true);
 
         graph.print();
 
-        String[] path = graph.shortestPath("0", "6");
+        String[] path = graph.shortestPath("0", "6", false);
 
         printPath(path);
-
+        /*
         graph.removeBuilding("3");
 
         graph.print();
 
         FileController.writeBuildings(graph.getBuildings());
+        */
     }
 
     private static void printPath(String[] path) {
