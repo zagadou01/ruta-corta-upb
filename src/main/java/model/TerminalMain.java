54757e979b15ff03c8a52ba6cb839cfec5288996
zagadou01 <ju_na2705@outlook.com>
2025-05-03 19:59:18 -0500

    Testing and fixing shortest path algorithm

--- src/main/java/model/TerminalMain.java
+++ src/main/java/model/TerminalMain.java
@@ -5,48 +5,23 @@ import controller.FileController;
 // TODO Main temporal para probar algoritmos y grafos por consola, se eliminará al final
 public class TerminalMain {
     public static void main(String[] args) {
-        Building a = new Building("0");
-        Place restaurant = new Place("Restaurant");
-        Place library = new Place("Library");
-        a.addPlace(restaurant);
-        a.addPlace(library);
-
         Graph graph = new Graph();
 
-        /*
-        graph.addBuilding(a);
-        graph.addBuilding(new Building("1"));
-        graph.addBuilding(new Building("2"));
-        graph.addBuilding(new Building("3"));
-        graph.addBuilding(new Building("4"));
-        graph.addBuilding(new Building("5"));
-        graph.addBuilding(new Building("6"));
-        */
+        graph.addBuilding(new Building("A"));
+        graph.addBuilding(new Building("B"));
+        graph.addBuilding(new Building("C"));
+        graph.addBuilding(new Building("J"));
 
-        graph.addBuildings(FileController.readBuildings());
-
-        graph.addRoute("0", "1", 2, true);
-        graph.addRoute("0", "2", 6, false);
-        graph.addRoute("1", "3", 5, true);
-        graph.addRoute("2", "3", 8, false);
-        graph.addRoute("3", "4", 9/*10*/, true);
-        graph.addRoute("3", "5", 9/*15*/, false);
-        graph.addRoute("4", "5", 6, false);
-        graph.addRoute("4", "6", 2, false);
-        graph.addRoute("5", "6", 6, true);
+        graph.addRoute("A", "B", 5, false);
+        graph.addRoute("B", "C", 0, true);
+        graph.addRoute("A", "J", 100, true);
+        graph.addRoute("C", "J", 0, true);
 
         graph.print();
 
-        String[] path = graph.shortestPath("0", "6", false);
+        String[] path = graph.shortestPath("A", "J", true);
 
         printPath(path);
-        /*
-        graph.removeBuilding("3");
-
-        graph.print();
-
-        FileController.writeBuildings(graph.getBuildings());
-        */
     }
 
     private static void printPath(String[] path) {
